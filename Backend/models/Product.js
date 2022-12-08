4eeec9ba5e1148e44580bb220d2dfba5162045c8
nbduc20176010 <duc.nb176010@sis.hust.edu.vn>
2022-12-08 20:27:27 +0700

    first commit

---
+++ Backend/models/Product.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        img: {
+            type: String,
+            required: true,
+        },
+        category: {
+            type: String,
+            required: true,
+        },
+        quantity: {
+            type: Number,
+            required: true,
+        },
+        price: {
+            type: Number,
+            required: true,
+        },
+        currency: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+        collection: "Products",
+    }
+);
+
+module.exports = mongoose.model("Product", productSchema);
---
+++ Frontend/README.md
@@ -0,0 +1,10 @@
+//fetch project to your PC :
+$ git clone https://github.com/nbduc20176010/BTL-coffee-shop-management.git
+
+install node module : npm i
+
+run app : npm start
+
+front-end : Nguyen Ba Duc
+back-end : Tran Tuan Anh
+
